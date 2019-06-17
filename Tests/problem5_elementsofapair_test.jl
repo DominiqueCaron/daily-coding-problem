95ddc15d6a1595875487f11a0b592521e3f0be0c
DominiqueCaron <dominique.caron.1@umontreal.ca>
2019-06-17 09:27:05 -0400

    challenge #5

---
+++ Tests/problem5_elementsofapair_test.jl
@@ -0,0 +1,18 @@
+#=
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+Implement car and cdr.
+=#
+
+using Test
+include("Solutions/problem5_elementsofapair.jl")
+
+@test car(cons(3, 4)) == 3
+@test cdr(cons(3, 4)) == 4
