87b7d9191a41453e501fb35487f8109f3cd68353
DominiqueCaron <dominique.caron.1@umontreal.ca>
2019-08-12 15:31:25 -0400

    Challenge #59

---
+++ Tests/problem59_file_syncing_algorithm_test.jl
@@ -0,0 +1,6 @@
+#=
+Implement a file syncing algorithm for two computers over a low-bandwidth network. What if we know the files in the two computers are mostly the same?
+=#
+
+using Test
+include("Solutions/problem59_file_syncing_algorithm.jl")
